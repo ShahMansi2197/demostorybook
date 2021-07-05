728a7093942b7a360d1fa2198a1f3938f94f2b68
shahmansi2197 <mansi.mobilefirst@gmail.com>
2021-07-05 16:14:04 +0530

    Nested Story & Args

--- src/Component/Button/Button.stories.tsx
+++ src/Component/Button/Button.stories.tsx
@@ -3,10 +3,28 @@ import Button from "./Button";
 
 export default {
     title : 'Form/Button',
-    component : Button
+    component : Button,
+    args : {
+        children : 'Button'
+    }
 }
 
-export const Primary = () => <Button variant="Primary">Primary</Button>
+export const Primary = () => <Button variant="primary">Primary</Button>
 export const Secondary = () => <Button variant="secondary">Secondary</Button>
-export const Success = () => <Button variant="success">Success</Button>
+export const Success = () => <Button variant="sucess">Success</Button>
 export const Danger = () => <Button variant="danger">Danger</Button>
+
+
+const Template = (args : any) => <Button {...args}/>
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args  =   {
+    variant : "primary",
+  //  children : 'Primary Args'
+};
+
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args ={
+    ...PrimaryA.args,
+  //  children : 'Long Primary Args'
+}
---
+++ src/Subscription/Subscription.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Primary } from "../Component/Button/Button.stories";
+import { Large } from "../Component/Input/Input.stories";
+
+export default{
+    title : 'Form/Subscription'
+}
+
+export const PrimarySubscription = () => (
+    <>
+    <Large />
+    <Primary />
+    </>
+)
